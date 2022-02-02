203cbf660b9180e70a0b3c0496f65ef2d8bce4bc
Django_hp820 <siabdel58@gmail.com>
2022-02-02 12:27:55 +0100

    add views  ContacteDetail et ContacteList

---
+++ src/views/ContacteDetails.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="container" >
+        <h2> Details du contacte {{ $route.params.contacte.nom }}  </h2>
+        <div>
+            <img class="img-rounded" src="../assets/avatar.jpg" alt="">
+        </div>
+        <div class="breadcrumb">
+            <p>
+                {{ $route.params.contacte.titre }} 
+            </p>    
+            <p>
+            Nom : {{ $route.params.contacte.nom }} {{ $route.params.contacte.prenom }} 
+            </p>
+            <p>
+            Email : {{ $route.params.contacte.email }}
+            </p>
+            <p>
+                Telephone : {{ $route.params.contacte.telephone }} 
+            </p>
+
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import 'bootstrap3/dist/css/bootstrap.min.css';
+export default ({
+    name : "UserDetails",
+
+    data() {
+      return {
+        message : "",
+      }
+    },
+
+    methods: {
+
+    }
+
+})
+
+</script>
