095db5c12b2731193b39cff7df252cfd167632f4
Django_hp820 <siabdel58@gmail.com>
2022-02-02 11:44:38 +0100

    Rename views : UserDetail et UserList en ContacteDetail et ContacteList

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
 import Vue from "vue"
 import Router from "vue-router"
 import Login from "./views/Login.vue";
-import UserList from "./views/UserList.vue";
-import UserDetails from "./views/UserDetails.vue";
+import ContacteList from "./views/ContacteList.vue";
+import ContacteDetails from "./views/ContacteDetails.vue";
 import Home from "./views/Home.vue";
 import authAPI from "./auth.js";
 
@@ -26,14 +26,14 @@ export default new Router({
     {
         name : "user_list", 
         path : "/users/list",
-        component : UserList,
+        component : ContacteList,
 
     },
 
     {
         name : "user",
         path : "/user/:userId",
-        component : UserDetails,
+        component : ContacteDetails,
         beforeEnter : (to, from, next) => {
             if( authAPI.isAuthentificated === false){
                 next('/login')
