2436e350e80820f39a0c0d17f0e68cb803f2eacc
Django_hp820 <siabdel58@gmail.com>
2022-02-03 15:04:38 +0100

    Corrections & fonction de validation form input email and password

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,62 +1,56 @@
 <template>
-    <div>
+  <div class="row" id="pwd-container">
+    <div class="col-md-4"></div>
+    
+    <div class="col-md-4">
+      <section class="login-form">
+        <form method="post" action="#" role="login" >
+          <img src="http://i.imgur.com/RcmcLv4.png" class="img-responsive" alt="" />
 
-        <div class="container" id="app_contacte" >
-            <div>
-            <ul class="list-inline">
-                <li class="list-inline-item"> <router-link to="/"> Home </router-link> </li>
-            </ul>   
-            </div>
-            <div class="row card" >
-                <div class="col-lg-8" >
-                    <div class="col-lg-8 text-center">
-                        <h2>Connexion </h2>
-                        <h2 v-show="loggedIn" >Connected </h2>
-                    </div>
-                    <div class="col-lg-2"><img src="../assets/logo.png" alt="logo">
-                    </div>
-                    <form name="sentMessage" id="contactForm" >
-                            <div class="col-md-8">
-                                <div class="form-group">
-                                    <input v-model="email" type="email" class="form-control" placeholder="Votre Email *" id="email" required data-validation-required-message="Merci de rentrer votre addresse email .">
-                                    <p class="help-block text-danger"></p>
-                                </div>
-                            </div>
-                            <div class="col-md-8">
-                                <div class="form-group">
-                                    <input v-model="password" type="password" class="form-control" 
-                                        placeholder="Votre mot de passe" id="password" 
-                                        required data-validation-required-message="Merci de rentrer votre password .">
-                                    <p class="help-block text-danger"></p>
-                                </div>
-                            </div>
-                            <div class="clearfix"></div>
-
-                            <div class="col-lg-12 mb-3 form-check">
-                                <div v-show="error" id="success" class="bg-info text-warning" >[[ error ]]</div>
-                                <button type="submit" @click.prevent="onLogin" class="btn btn-success btn-lg">Connectez</button>
-                            </div>
-                            <div v-show="!showForm" class="col-lg-12 text-center">
-                                <div v-show="error" id="success" class="bg-success text-warning" > <h3>[[ message ]] </h3>
-                                <img class="img" src="static/images/clin_oeil_anime.jpeg" alt="" width="100" height="50"></div>
-                            </div>
-                    </form>
-                </div>
-            </div>
+          <input type="email" v-model="email" placeholder="Email" 
+            required class="form-control input-lg"   />
+          
+          <input type="password" v-model="password" class="form-control input-lg"  
+            placeholder="Password" required="" />
+          
+          
+          <div class="pwstrength_viewport_progress"></div>
+          
+          
+          <button type="submit" @click.prevent="onLogin"  name="go" 
+          class="btn btn-lg btn-primary btn-block">Se conneter</button>
+          <div>
+            <a href="#">Create account</a> or <a href="#">reset password</a>
+          </div>
+          <div>
+            <ul>
+              <li v-for="(error, index) in errors"  :key=index class="text-warning"> {{ error }} !!</li>
+            </ul>
+          </div>
+          
+        </form>
+        
+        <div class="form-links">
+          <a href="#">www.website.com</a>
         </div>
+      </section>  
+      </div>
+      
+      <div class="col-md-4"></div>
+      
 
-    </div>
-</template>
-
+  </div> </template>
 <script>
 import authAPI from  "../auth";
+// Regular expression from W3C HTML5.2 input specification:
+var emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 export default ({
     name : "Login",
     data : () => ({
         message : "",
         email : null,
         password: null,
-        error : false, 
+        errors : [], 
         showForm : true,
         isSaved : false,
         loggedIn : false,
@@ -64,19 +58,53 @@ export default ({
     methods :{
         
         onLogin(){
+          if(this.isEmail(this.email) && this.isPassword(this.password) ){
+            this.apiLogin()
+          }else{
+            console.log("votre email  n'est pas valide !!!")
+          }
+        },
+
+        apiLogin(){
+            //validate saisie email, passwd 
             authAPI.login(this.email, this.password)
             .then( () =>  {
                 this.$router.push("/")
-                this.error = false;
                 console.log("Connected !!" + this.email + " et " + this.password)
+                this.errors = []
             })
             .catch( () => {
-                this.error = true;
-                this.message = "Erreur de connexion !!"
-                console.log("Erreur de connexion !!" + this.email + this.password)
+                this.errors = []
+                console.log("Api Erreur de connexion !!" + this.email + this.password)
+                this.errors.push("Il y a une erreur dans votre adresse email ou le mot de passe")
             });
+        },
+
+        // check for valid email adress
+        isEmail(value) {
+          if (emailRegExp.test(value)){
+            return true
+          }else {
+                this.errors.push("Vote email n'est pas valide ")
+          }
+        },
+
+        isPassword(passwd){
+          if(!this.password || this.password.length < 5){
+            this.errors.push("password non valide nbre car < 5 !!") 
+            console.log("password non valide nbre car < 5 !!" + passwd)
+            return false
+          }else {
+            return true
+          }
+        },
+        // watching nested property
+        validate_email(event) {
+          console.log("computed validate email" + event.value )
+          return this.isEmail("email", event.value) 
         }
-    }
+      },
+      
     
 })
 </script>
